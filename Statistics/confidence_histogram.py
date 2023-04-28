028df62ff4a7bab61cabc8e3b63b5490b32fca8e
benjaminfuller <benjamin.w.fuller@gmail.com>
2023-04-28 11:08:22 -0400

    computing feature quality

--- Statistics/confidence_histogram.py
+++ Statistics/confidence_histogram.py
@@ -5,6 +5,7 @@ import csv
 
 p_same=[]
 p_diff=[]
+p_gap=[]
 with open('../PythonImpl/dense_0.205_57-75.txt', newline='') as csvfile:
     confidence_info = csv.reader(csvfile, delimiter=' ', quotechar='|')
     
@@ -12,13 +13,24 @@ with open('../PythonImpl/dense_0.205_57-75.txt', newline='') as csvfile:
         if len(row)>7:
             p_same.append(float(row[6]))
             p_diff.append(float(row[7]))
+            p_gap.append(float(row[8]))
 
 #print(p_same)
 #bins=[.05,.075,.1,.125,.15,.175,.2,.225.25,.30,.35,.4,.45,.5,.55,.60]
 n, bins, patches = plt.hist([p_same,p_diff], bins=20, color=["Blue","Red"],alpha=0.5)
+
 plt.xlabel("Error Rate")
 plt.ylabel("Frequency")
 plt.title("Histogram of Error Rates of Individual Features")
 plt.legend(["Same Biometric", "Different Biometrics"])
 plt.show()
+
+n, bins, patches = plt.hist(p_gap, bins=20, color="Blue",alpha=0.5)
+plt.xlabel("Gap between Error Rates")
+plt.ylabel("Frequency")
+plt.title("Histogram of Gap between psame and pdiff for Individual Features")
+plt.show()
+
+
+
 
