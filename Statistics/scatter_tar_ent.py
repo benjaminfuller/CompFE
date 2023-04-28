1d69e3a4b9ba7e0463b61471f6008731f559fd50
benjaminfuller <benjamin.w.fuller@gmail.com>
2023-04-28 11:50:00 -0400

    scatter ent tar

--- Statistics/scatter_tar_ent.py
+++ Statistics/scatter_tar_ent.py
@@ -3,24 +3,32 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import csv
 
-subset_size=[]
 tar=[]
 ent=[]
 alpha=[]
+subset_dict={}
 
 with open('ent_tar.txt', newline='') as csvfile:
     tar_rates = csv.reader(csvfile, delimiter='\t', quotechar='|')
     next(tar_rates)
     for row in tar_rates:
         if len(row)>3:
-            subset_size.append(int(row[0]))
-            alpha.append(int(row[1]))
+            subset_size = int(row[0])
+            if subset_size in subset_dict:
+                ent, tar = subset_dict[subset_size]
+            else:
+                ent=[]
+                tar=[]
+                subset_dict[subset_size]=(ent,tar)
             ent.append(float(row[2]))
             tar.append(float(row[3]))
                                
-
-plt.scatter(ent, tar, alpha=0.5)
+for key in subset_dict:
+    ent, tar = subset_dict[key]
+    plt.scatter(ent, tar, alpha=0.5, label="Subset Size = "+str(key))
+    
 plt.xlabel("Minimum of Entropy of across 10 subsets")
+plt.legend()
 plt.ylabel("True Accept Rate")
 plt.title("Entropy and TAR across subset sizes and alpha parameters")
 plt.show()
